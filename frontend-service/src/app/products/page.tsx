e5b2e27760f907cfc45bd6bf002a83e405f8b487
Ricardo Sanchez <ricsanfre@gmail.com>
2026-05-15 11:06:15 +0200

    Fixing Nextjs issue with missing size properties for the images

--- frontend-service/src/app/products/page.tsx
+++ frontend-service/src/app/products/page.tsx
@@ -65,6 +65,7 @@ export default async function ProductsPage() {
                   src={p.imageUrl}
                   alt={p.name}
                   fill
+                  sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 33vw"
                   style={{ objectFit: "cover", borderRadius: "0.25rem" }}
                 />
               </div>
